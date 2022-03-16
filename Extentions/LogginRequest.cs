a8b0f52d05eabe02a4ac25edcf519057bba0fb8d
Daniel Komonii <danielkomonii@gmail.com>
2022-03-16 16:21:32 +0100

    Добавьте файлы проекта.

---
+++ Extentions/LogginRequest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace WebProject1
+{
+    public static class LogginRequest
+    {
+        public static IApplicationBuilder LogginUser(this IApplicationBuilder builder) 
+        {
+            return builder.UseMiddleware<UseLoggin>();
+        }
+
+    }
+}
---
+++ Middleware/UseLoggin.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+
+namespace WebProject1
+{
+    public class UseLoggin
+    {
+        private readonly RequestDelegate _next;
+        public UseLoggin(RequestDelegate next)
+        {
+            this._next = next;
+        }
+      
+        public async Task Invoke(HttpContext context)
+        {
+            var tocen = context.Request.Query["tocen"];
+            string path = context.Request.Path.Value.ToLower();
+            if (path=="/")
+            {
+                if (tocen != "Daniel")
+                {
+                    context.Response.StatusCode = 403;
+
+
+                }
+                else
+                {
+                    await _next.Invoke(context);
+                }
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+            
+        }
+        
+    }
+}
